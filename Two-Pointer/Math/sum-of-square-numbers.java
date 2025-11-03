94ee2e93d5d2476228431542cf0a7cc5fc3ce3ee
kharizz7 <116253531+kharizz7@users.noreply.github.com>
2025-11-03 23:36:43 +0530

    Create sum-of-square-numbers.java

---
+++ Two-Pointer/Math/sum-of-square-numbers.java
@@ -0,0 +1,30 @@
+Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+Example 1:
+
+Input: c = 5
+Output: true
+Explanation: 1 * 1 + 2 * 2 = 5
+
+Example 2:
+
+Input: c = 3
+Output: false
+
+  class Solution {
+    public boolean judgeSquareSum(int c) {
+        long b = (long) Math.sqrt(c);
+        long a=0;
+        while(a<=b){
+        long sum = (a*a)+(b*b);
+        if(sum==c){
+            return true;
+        }else if(sum<c){
+            a++;
+        }else{
+            b--;
+        }
+        }
+        return false;
+
+    }
+}
